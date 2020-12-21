090b376b7b84f41f8d92b9133842133951eadfa5
przemyslaw wendeholm <pwenderholm@gmai.com>
2020-12-21 10:08:58 +0100

    first task version

---
+++ zadanieApi/script.js
@@ -0,0 +1,32 @@
+const divInfo = document.getElementById("info-div");
+function createInformation(information) {
+    const divNew = document.createElement("div");
+    divNew.innerHTML = information;
+    divNew.classList.add("mb-4");
+    divInfo.appendChild(divNew);
+//    return option;
+}
+
+
+function generateInfo() {
+const numberInfo = document.getElementById("facts_number").value;
+const animalType = document.getElementById("animal").value;
+    axios.get(`https://cat-fact.herokuapp.com/facts/random?animal_type=${animalType}&amount=${numberInfo}`).then(response => {
+//         usówanie starych elementów przed generacja nowego
+           while (divInfo.firstChild) {
+                divInfo.firstChild.remove();
+           };
+
+          if (numberInfo == ''){
+            alert('wpisz wartość w polu ilość ciekawostek z przedziału 1 do 10');
+//            break;
+          }else if (numberInfo == 1){
+            createInformation(response.data.text);
+          } else{
+            response.data.map(function(c){
+            createInformation(c.text);
+            });
+          };
+    });
+};
+
